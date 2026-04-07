fd881e425679cac748af5bd030c0bd05f19c5762
João Gabriel <joaogabriel@lifemed.ai>
2026-04-07 16:03:45 -0300

    ajustando rotas

--- routes/routes.py
+++ routes/routes.py
@@ -3,29 +3,29 @@ from services import s3_service, audit
 from services.s3_service import S3Service
 from services.audit import S3Auditor
 
-router = APIRouter()
+router = APIRouter(prefix="/file")
 s3_service = S3Service() # Uma única instância para o router todo usar
 auditor = S3Auditor(s3_service.client, s3_service.bucket_name) 
 s3_manager = s3_service
 
 
-@router.post("/upload/")
+@router.post("/")
 async def upload(file: UploadFile):
     content = await file.read()
     response = s3_service.upload(file.filename, content)
     return {"version_id": response.get("VersionId")}
 
-@router.get("/versions/{filename}")
+@router.get("/{filename}/versions")
 def versions(filename: str):
     res = s3_service.list_versions(filename)
     return res.get('Versions', [])
 
-@router.post("/restore/{filename}/{version_id}")
+@router.post("/{filename}/versions/{version_id}/restore")
 def restore(filename: str, version_id: str):
     s3_service.restore_version(filename, version_id)
     return {"message": f"Arquivo {filename} restaurado para a versão {version_id}"}
 
-@router.get("/download/{filename}")
+@router.get("/{filename}/download")
 def download_file(filename: str, version_id: str = None):
     conteudo = s3_manager.buscar_arquivo(filename, version_id)
     if conteudo is None:
@@ -33,7 +33,7 @@ def download_file(filename: str, version_id: str = None):
     
     return {"filename": filename, "content": conteudo}
 
-@router.delete("/delete/{filename}")
+@router.delete("/{filename}")
 def delete_file(filename: str):
     try:
         s3_manager.client.delete_object(Bucket=s3_manager.bucket_name, Key=filename)
@@ -41,6 +41,6 @@ def delete_file(filename: str):
     except Exception as e:
         return {"error": str(e)}
     
-@router.get("/audit/{filename}")
+@router.get("/{filename}/audit")
 def audit_file(filename: str):
     return auditor.gerar_auditoria(filename)
